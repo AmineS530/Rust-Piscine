1a1cad6af3bf184f189a72b4a31208b1faa8b481
Sadik Amine <idekwiam18@gmail.com>
2025-07-25 21:44:39 +0100

    matrix_transposistion

---
+++ 01_Variables/looping/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut counter = 1;
+    let riddle =
+        "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
+    loop {
+        let mut input = String::new();
+        println!("{}", riddle);
+        std::io::stdin().read_line(&mut input).unwrap();
+        if input.trim() == "The letter e" {
+            break;
+        }
+        counter += 1;
+    }
+    println!("Number of trials: {}", counter);
+}
