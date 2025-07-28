c9a99d3bb3a54cd5f0583ec17a53cd8743a78b3d
Sadik Amine <idekwiam18@gmail.com>
2025-07-28 11:18:20 +0100

    to be continued

--- borrow_me_the_reference/src/lib.rs
+++ borrow_me_the_reference/src/lib.rs
@@ -1,3 +1,36 @@
-pub fn delete_and_backspace(s: &mut String) {}
+pub fn delete_and_backspace(s: &mut String) {
+    let mut res = String::new();
+    for char in s.chars() {
+        if char == '+' {
+            char.next();
+            continue;
+        } else if char == '-' {
+            res.pop();
+            continue;
+        }
+        res.push(char);
+    }
+    *s = res;
+}
 
-pub fn do_operations(v: &mut [String]) {}
+// pub fn do_operations(v: &mut [String]) {}
+// pub fn delete_and_backspace(s: &mut String) {
+//     let mut res = String::new();
+//     let mut chars = s.chars().peekable();
+
+//     while let Some(c) = chars.next() {
+//         match c {
+//             '-' => {
+//                 res.pop(); // remove last added char
+//             }
+//             '+' => {
+//                 chars.next(); // skip the next character
+//             }
+//             _ => {
+//                 res.push(c);
+//             }
+//         }
+//     }
+
+//     *s = res;
+// }
