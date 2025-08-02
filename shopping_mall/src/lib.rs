408016109c940a934969177d7b96c2a0129503ed
Amine Sadik <idekwiam18@gmail.com>
2025-08-02 21:49:30 +0100

    to be back to shopping mall

--- shopping_mall/src/lib.rs
+++ shopping_mall/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub mod mall;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use mall::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn biggest_store(mall: &Mall) -> Store {
+    // todo
 }
