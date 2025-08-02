bf89d466c69c027007012c1897c5710b25a2a52d
Amine Sadik <idekwiam18@gmail.com>
2025-08-02 13:46:26 +0100

    middle_day

---
+++ middle_day/src/main.rs
@@ -0,0 +1,8 @@
+use middle_day::*;
+
+fn main() {
+    println!("{:?}", middle_day(1022));
+    println!("{:?}", middle_day(2044));
+    println!("{:?}", middle_day(2018));
+    println!("{:?}", middle_day(2024));
+}
