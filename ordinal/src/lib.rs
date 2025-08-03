4b438680e80b3498eb819be816bc86cef486db88
Amine Sadik <idekwiam18@gmail.com>
2025-08-03 06:55:29 +0100

    fixed ordinal

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,8 +1,8 @@
 pub fn num_to_ordinal(x: u32) -> String {
-    if x % 100 >= 11 && x % 100 <= 13 {
+    if (11..=13).contains(&(x % 100)) {
         format!("{}th", x)
     } else {
-        match x {
+        match x % 10 {
             1 => format!("{}st", x),
             2 => format!("{}nd", x),
             3 => format!("{}rd", x),
