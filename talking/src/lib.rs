bcb954a8d59512387ba6c5c7980b0c2a2368ad35
Sadik Amine <idekwiam18@gmail.com>
2025-08-04 18:44:13 +0100

    talking

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -7,18 +7,20 @@ pub fn talking(text: &str) -> &str {
 
     let is_question = trimmed.ends_with('?');
     let mut all_upper = true;
+    let mut has_alpha = false;
 
     for c in trimmed.chars() {
         if c.is_alphabetic() {
+            has_alpha = true;
             if !c.is_uppercase() {
                 all_upper = false;
             }
         }
     }
 
-    if all_upper && is_question {
+    if has_alpha && all_upper && is_question {
         "Quiet, I am thinking!"
-    } else if all_upper {
+    } else if has_alpha && all_upper {
         "There is no need to yell, calm down!"
     } else if is_question {
         "Sure."
