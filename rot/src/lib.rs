2eb3408f6f0b6cd6ed5c3f40d08e46a939e00ff5
Sadik Amine <idekwiam18@gmail.com>
2025-08-04 22:20:06 +0100

    fixed

--- rot/src/lib.rs
+++ rot/src/lib.rs
@@ -1,5 +1,6 @@
 pub fn rotate(input: &str, key: i8) -> String {
     let mut result = String::new();
+
     for c in input.chars() {
         result.push(caesar_cipher(c, key));
     }
@@ -7,6 +8,8 @@ pub fn rotate(input: &str, key: i8) -> String {
 }
 
 fn caesar_cipher(c: char, key: i8) -> char {
+    let key = ((key % 26 + 26) % 26) as u8;
+
     match c {
         'A'..='Z' => ((c as u8 - b'A' + key as u8) % 26 + b'A') as char,
         'a'..='z' => ((c as u8 - b'a' + key as u8) % 26 + b'a') as char,
